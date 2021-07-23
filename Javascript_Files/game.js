2b9946c0fd317c27b11c05d8afc6dfb0d6b3a54a
Zim Agu <zimmagu@gmail.com>
2021-07-22 18:49:18 -0700

    Sky is successfully copied on to canvas

--- Javascript_Files/game.js
+++ Javascript_Files/game.js
@@ -11,8 +11,17 @@ const context = canvas.getContext("2d");
 loadImage("/Spritesheet_Images/world.png")
     .then(image => {
         const worldSprite = new spriteSheet(image, 16, 16);
+
         worldSprite.saveTheSprite('ground', 0, 0);
-        worldSprite.drawTheSprite('ground', context, 50, 50);
+        worldSprite.saveTheSprite('sky', 3, 23)
+        
+
+        for (let screenColumns = 0; screenColumns < 50; screenColumns++) {          // This for loop represents the width of the canvas
+            for (let screenRows = 0; screenRows < 25; screenRows++) {               //This for loop represents the height of the canvas
+                worldSprite.drawTheSprite('sky', context, screenColumns * 16, screenRows * 16);
+            }
+        }
+        
     })
     .catch((error) => {
         console.log('Image could not be loaded because of this error ', error)
