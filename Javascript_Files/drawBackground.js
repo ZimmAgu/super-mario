8711d8d71df62924b77996723cb6d063036df088
Zim Agu <zimmagu@gmail.com>
2021-07-23 11:38:07 -0700

    Characters sprite sheet inbound

--- Javascript_Files/drawBackground.js
+++ Javascript_Files/drawBackground.js
@@ -1,15 +1,15 @@
 
-function drawBackgroundImages (background, spriteContext, currentSprite) { // Draws the current sprite across the specified distance in the canvas in the background
-    background.dimensions.forEach( ([colStart, colEnd, rowStart, rowEnd]) => { // The array stuffed in the parameter is where the dimensions from the levels JSON files will be stored
-        for (let screenColumns = colStart; screenColumns < colEnd; screenColumns++) {          // This for loop represents the width of the canvas
-            for (let screenRows = rowStart; screenRows < rowEnd; screenRows++) {
+function drawBackgroundImages (background, spriteContext, currentSprite) {                  // Draws the current sprite across the specified distance in the canvas in the background
+    background.dimensions.forEach( ([colStart, colEnd, rowStart, rowEnd]) => {              // The array stuffed in the parameter is where the dimensions from the levels JSON files will be stored
+        for (let screenColumns = colStart; screenColumns < colEnd; screenColumns++) {       // This for loop represents how wide the sprite will be drawn on the canvase
+            for (let screenRows = rowStart; screenRows < rowEnd; screenRows++) {            // This loop represents how tall the sprite will be drawn on the canvas
                 currentSprite.drawTexture(background.name, spriteContext, screenColumns, screenRows)
             }
         } 
     })
-
-    // console.log('Background from drawing thingy: ', background.dimensions)
 }
+
+
 
 function drawBackground (gameLevel, spriteContext, allSprites) {    // Combines the background images together into one single background
     gameLevel.backgrounds.forEach(levelBackground => {
--- Javascript_Files/game.js
+++ Javascript_Files/game.js
@@ -1,5 +1,5 @@
 import drawBackground from "./drawBackground.js";
-import {loadImage, loadLevel, loadBackgroundSprites} from "./loadFunctions.js";
+import {loadLevel, loadBackgroundSprites} from "./loadFunctions.js";
 
 
 
@@ -10,6 +10,6 @@ const context = canvas.getContext("2d");
 Promise.all([   // Will make the Spritesheet and world textures load at the same time instead of one after another
     loadBackgroundSprites(),
     loadLevel('1-1')
-]).then(([image, level]) => {   // The image parameter is what is returned from the loadBackgroundSprites() function. The level parameter is what is returned from the loadLevel() function
-    drawBackground(level, context, image)
+]).then(([backgroundSprites, level]) => {   // The image parameter is what is returned from the loadBackgroundSprites() function. The level parameter is what is returned from the loadLevel() function
+    drawBackground(level, context, backgroundSprites)
 })
