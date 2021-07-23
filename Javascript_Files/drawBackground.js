6201622568174111ce23d8deb218ba2f388adedc
Zim Agu <zimmagu@gmail.com>
2021-07-23 15:33:29 -0700

    Added a text file explaining my JSON file

--- Javascript_Files/drawBackground.js
+++ Javascript_Files/drawBackground.js
@@ -1,12 +1,12 @@
 "use strict";
 function drawBackgroundImages (background, spriteContext, currentSprite) {                  // Draws the current sprite across the specified distance in the canvas in the background
-    const SCREENSIZE = 2
+    const SPRITE_RATIO = 2
     
     background.dimensions.forEach( ([colStart, colEnd, rowStart, rowEnd]) => {              // The array stuffed in the parameter is where the dimensions from the levels JSON files will be stored
         for (let screenColumns = colStart; screenColumns < colEnd; screenColumns++) {       // This for loop represents how wide the sprite will be drawn on the canvase
             for (let screenRows = rowStart; screenRows < rowEnd; screenRows++) {            // This loop represents how tall the sprite will be drawn on the canvas
-                const onScreenColumnSize = screenColumns * SCREENSIZE;
-                const onScreenRowSize   = screenRows * SCREENSIZE;
+                const onScreenColumnSize = screenColumns * SPRITE_RATIO;
+                const onScreenRowSize   = screenRows * SPRITE_RATIO;
                 currentSprite.drawTexture(background.name, spriteContext, onScreenColumnSize, onScreenRowSize)
             }
         } 
