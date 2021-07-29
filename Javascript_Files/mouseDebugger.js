ce6d069dc408ec7077df74b13f251bf835c13173
Zim Agu <zimmagu@gmail.com>
2021-07-28 20:07:34 -0700

    Camers can scroll on right click. It can't do it very well but it can do it

--- Javascript_Files/mouseDebugger.js
+++ Javascript_Files/mouseDebugger.js
@@ -1,16 +1,26 @@
 
 function mouseControl (canvas, object, camera) {
+    let previousEvent;
     ['mousedown', 'mousemove'].forEach(eventName => {
         canvas.addEventListener(eventName, event => {
-            if (event.buttons == 1) {
+            if (event.buttons == 1) { // Left click
                 object.velocity.setVector(0, 0);
                 object.position.setVector(
                                 event.offsetX + camera.position.x, 
                                 event.offsetY + camera.position.y
                             );
+            } else if (event.buttons == 2 // Right clisk
+                        && previousEvent && previousEvent.buttons === 2 
+                        && previousEvent.type === 'mousemove') {
+                camera.position.x -= event.offsetX - previousEvent.offsetY;
             }
+            previousEvent = event;
         })
     })
+
+    canvas.addEventListener('contextmenu', event => {
+        event.preventDefault();
+    });
 }
 
 export default mouseControl;
