5b90d3ccf5eeb47cba0368e71be1d757be075147
Zim Agu <zimmagu@gmail.com>
2021-08-02 11:16:40 -0700

    Mario has taken his first steps

--- Javascript_Files/Classes/MoveTrait.js
+++ Javascript_Files/Classes/MoveTrait.js
@@ -6,11 +6,20 @@ class Move extends Trait {
         super('move');
 
         this.movementDirection = 0; // How long the jump lasts
-        this.movementSpeed = 6000;
+        this.movementSpeed = 6000;  // How fast mario moves
+
+        this.distance = 0;
     }
 
     updateTrait (object, elapsedTime) { // Holds all of the functionality of a jump
         object.velocity.x = this.movementSpeed * this.movementDirection * elapsedTime;
+
+        if (this.movementDirection) { // If the character is moving, then the distance variable is incremented up regardless of what direction the character is moving in
+            this.distance += Math.abs(object.velocity.x) * elapsedTime;
+        } else {    // If the character is not moving, the distance gets reset to 0
+            this.distance = 0;
+        }
+        
     }
 }
 
--- Javascript_Files/createMario.js
+++ Javascript_Files/createMario.js
@@ -18,13 +18,31 @@ function createMario () {
             mario.size.setVector(32, 42.6);
             mario.addTrait(new Move())
             mario.addTrait(new Jump());  
-                   
+
+
+            const marioFrames = [
+                'normal mario run1',
+                'normal mario run2',
+                'normal mario run3'
+            ]
+            
+            
+            function routeFrame () {
+                if (mario.move.movementDirection !== 0) {
+                    console.log(Math.floor(mario.move.distance / 10)  % marioFrames.length);
+                    const currentFrame = Math.floor(mario.move.distance / 10)  % marioFrames.length;
+                    return marioFrames[currentFrame];
+                } 
+
+                return 'normal mario idle'
+            }       
+
 
             mario.position.setVector(64, 100);   // Sets the position of mario
             const positionOfMario = mario.position;
 
             mario.drawObject = (context) => {
-                marioSprite.drawTheSprite('idle normal mario', context, 0, 0);
+                marioSprite.drawTheSprite(routeFrame(this), context, 0, 0);
             }
 
             return mario;
