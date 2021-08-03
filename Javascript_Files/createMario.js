3b095c5f8ce658f772d2fbf188271f547b469af0
Zim Agu <zimmagu@gmail.com>
2021-08-03 12:31:56 -0700

    Breaking animation added to mario

--- Javascript_Files/createMario.js
+++ Javascript_Files/createMario.js
@@ -21,25 +21,34 @@ function createMario () {
 
         function routeFrame () {
             let sign = Math.sign(mario.move.movementDirection)
-            
-            if (mario.move.movementDirection !== 0 && sign === 1) {
+
+            if (mario.move.movementDirection === -1 && mario.velocity.x > 0) {  // Break animation runs if user abruptly turns left after going right
+                return 'break facing left';
+            }
+
+            if (mario.move.movementDirection === 1 && mario.velocity.x < 0) { // Break animation runs if user abruptly turns right  after going left
+                return 'break facing right';
+            }
+
+            if (mario.move.movementDirection !== 0 && sign === 1) {         // Mario running animation loops when user presses and holds the button to go right
                 return marioRunningRight(mario.move.distance);
             } 
 
-            if (mario.move.movementDirection !== 0 && sign === -1) {
+            if (mario.move.movementDirection !== 0 && sign === -1) {        // Mario running animation loops when user presses and holds the button to go left
                 return marioRunningLeft(mario.move.distance);
             }
 
 
             if (mario.move.heading === 1) {
+
                 if (mario.velocity.x > 0) {
-                    return marioRunningRight(mario.move.distance);
+                    return marioRunningRight(mario.move.distance); // Mario will take a couple of extra steps before stopping completely
                 }
 
                 return 'normal mario idle right'
             } else {
                 if (mario.velocity.x < 0) {
-                    return marioRunningLeft(mario.move.distance);
+                    return marioRunningLeft(mario.move.distance); // Mario will take a couple of extra steps before stopping completely
                 }
                 return 'normal mario idle left'
             }
