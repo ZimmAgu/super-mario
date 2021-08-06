768e523d57f4ccb19d6c1e229b8582730dadd6d1
Zim Agu <zimmagu@gmail.com>
2021-08-06 13:21:32 -0700

    Koopa has walking animations

--- Javascript_Files/Animations/koopaAnimations.js
+++ Javascript_Files/Animations/koopaAnimations.js
@@ -2,11 +2,27 @@
 import { createAnimation } from "../createAnimation.js";
 
 
+const koopaWalkingRight = createAnimation(
+    [
+        "koopa right 3",
+        "koopa right 4"
+    ],
+    0.2
+)
+
+const koopaWalkingLeft = createAnimation(
+    [
+        "koopa left 3",
+        "koopa left 4"
+    ],
+    0.2
+)
+
 function routeKoopaFrame (object) {
     if (object.velocity.x > 0){
-        return "koopa right 1"    
+        return koopaWalkingRight(object.lifetime)    
     } else {
-        return "koopa left 1"
+        return koopaWalkingLeft(object.lifetime)  
     }
         
 }
