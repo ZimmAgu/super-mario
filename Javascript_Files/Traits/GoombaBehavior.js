dcd3f2da7c30b0e26531b951f97e792cda1f57e9
Zim Agu <zimmagu@gmail.com>
2021-08-07 17:53:01 -0700

    Goomba bounce functionality works

--- Javascript_Files/Traits/GoombaBehavior.js
+++ Javascript_Files/Traits/GoombaBehavior.js
@@ -9,6 +9,10 @@ class GoombaBehavior extends Trait {
     }
 
     collides (goomba, otherCharacter) {
+        if (goomba.ableToDie.isDead == true) {
+            return;    // If the goomba is already dead then mario can not bounce on it anymor
+        }
+
         if (otherCharacter.stomp) {                 // Mario is the only character with a trait named stomp so the goomba will only react to a collision with mario. Not any other characters
             goomba.pendelumWalk.walkSpeed = 0;      // The goomba will stop in place
             goomba.ableToDie.dies();                // The goombas death trait will be set to tru
