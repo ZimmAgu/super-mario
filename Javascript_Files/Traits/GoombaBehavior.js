a6f675ad3c91152c1dab173481f9689d3604cceb
Zim Agu <zimmagu@gmail.com>
2021-08-07 17:48:09 -0700

    Mario bounces upwards after stomping on Goomba

--- Javascript_Files/Traits/GoombaBehavior.js
+++ Javascript_Files/Traits/GoombaBehavior.js
@@ -9,9 +9,10 @@ class GoombaBehavior extends Trait {
     }
 
     collides (goomba, otherCharacter) {
-        if (otherCharacter.stomp) { // Mario is the only character with a trait named stomp so the goomba will only react to a collision with mario. Not any other characters
-            goomba.pendelumWalk.walkSpeed = 0; 
-            goomba.ableToDie.dies();
+        if (otherCharacter.stomp) {                 // Mario is the only character with a trait named stomp so the goomba will only react to a collision with mario. Not any other characters
+            goomba.pendelumWalk.walkSpeed = 0;      // The goomba will stop in place
+            goomba.ableToDie.dies();                // The goombas death trait will be set to tru
+            otherCharacter.stomp.bounceUpward()
         }
     }
 }
--- Javascript_Files/Traits/Stomp.js
+++ Javascript_Files/Traits/Stomp.js
@@ -4,6 +4,19 @@ import Trait from "./Traits.js";
 class Stomp extends Trait {
     constructor () {
         super('stomp');
+        this.bounce = false;
+        this.bounceSpeed = 500;
+    }
+
+    bounceUpward () {
+        this.bounce = true;
+    }
+
+    updateTrait (object) {
+        if (this.bounce) {
+            object.velocity.y = -this.bounceSpeed; // If the bounce method is called the object will fly upward at bounceSpeed
+            this.bounce = false
+        }
     }
 }
 
