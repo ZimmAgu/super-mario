9730a567eff0b31aa5cd56d55f0bc093f2741279
Zim Agu <zimmagu@gmail.com>
2021-08-09 19:11:57 -0700

    Mario World Time on screen

--- Javascript_Files/DrawTheLayers/drawDashboardLayer.js
+++ Javascript_Files/DrawTheLayers/drawDashboardLayer.js
@@ -1,7 +1,9 @@
 "use strict";
 
 function drawDashboardLayer (font, context) {
-    font.printWord("MARIO", context, 16, 8);
+    font.printWord("MARIO", context, 32, 16);
+    font.printWord("WORLD", context, 256, 16);
+    font.printWord("TIME", context, 384, 16);
 }
 
 export default drawDashboardLayer;
